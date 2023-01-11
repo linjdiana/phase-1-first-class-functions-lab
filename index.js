726bc1f54392fb8e7909bb558ba9304fff6e2c01
triciahughes <hughes.a.tricia@gmail.com>
2023-01-11 15:23:05 -0800

    created two functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+// function returnFirstTwoDrivers() {
+//   return;
+// }
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = () => {
+  return drivers.splice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(2);
+};
